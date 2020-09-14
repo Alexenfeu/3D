f41172a9d5d58c19cbe31f17cd0190a266c8381f
Alexenfeu <alexissarlat@gmail.com>
2020-09-14 17:06:39 +0200

    tower + ball qui roule

---
+++ Assets/Scripts/Project Tower/BallOfLife.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallOfLife : MonoBehaviour
+{
+    public float vitesse = 15f;
+
+    Transform target;
+    int wayPointIndex = 0;
+    void Start()
+    {
+        target = WayPoints.points[0];
+    }
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * vitesse * Time.deltaTime, Space.World);
+        if(Vector3.Distance(transform.position, target.position) <= 0.2)
+        {
+            GetNextWayPoint();
+        }
+    }
+    void GetNextWayPoint()
+    {
+        wayPointIndex++;
+        if(wayPointIndex == 4)
+        {
+            wayPointIndex = 0;
+        }
+        target = WayPoints.points[wayPointIndex];
+    }
+}
